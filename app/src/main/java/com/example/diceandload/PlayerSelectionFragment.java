304c5ddec8978a353713ba0496324e820beffe63
snehith1769 <snehithb57624@gmail.com>
2020-02-29 00:32:21 +0530

    fragment changes

---
+++ app/src/main/java/com/example/diceandload/PlayerSelectionFragment.java
@@ -0,0 +1,65 @@
+package com.example.diceandload;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class PlayerSelectionFragment extends Fragment implements View.OnClickListener{
+
+    private PlayerSelectionViewModel mViewModel;
+
+    public static PlayerSelectionFragment newInstance() {
+        return new PlayerSelectionFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.player_selection_fragment, container, false);
+        final MainActivity mainActivity = (MainActivity) getActivity();
+        Button button = (Button)  v.findViewById(R.id.two);
+        Button button1 = (Button)  v.findViewById(R.id.three);
+        Button button2 = (Button)  v.findViewById(R.id.four);
+
+        button.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // implements your things
+        MainActivity mainActivity = (MainActivity) getActivity();
+        switch (v.getId()){
+            case R.id.two:
+                mainActivity.playerCount(2);
+                break;
+            case R.id.three:
+                mainActivity.playerCount(3);
+                break;
+            case R.id.four:
+                mainActivity.playerCount(4);
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(PlayerSelectionViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/diceandload/PlayerSelectionViewModel.java
@@ -0,0 +1,7 @@
+package com.example.diceandload;
+
+import androidx.lifecycle.ViewModel;
+
+public class PlayerSelectionViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
